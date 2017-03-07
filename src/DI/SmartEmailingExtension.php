a0aa331806bb84c988664b5dab1e04759727b4d7
attreid <attreid@gmail.com>
2017-03-07 08:40:03 +0100

    uprava di

--- src/DI/SmartEmailingExtension.php
+++ src/DI/SmartEmailingExtension.php
@@ -5,7 +5,7 @@ declare(strict_types = 1);
 namespace NAttreid\SmartEmailing\DI;
 
 use NAttreid\Cms\Configurator\Configurator;
-use NAttreid\Cms\ExtensionTranslatorTrait;
+use NAttreid\Cms\DI\ExtensionTranslatorTrait;
 use NAttreid\SmartEmailing\Hooks\SmartEmailingHook;
 use NAttreid\SmartEmailing\SmartEmailingClient;
 use NAttreid\WebManager\Services\Hooks\HookService;
